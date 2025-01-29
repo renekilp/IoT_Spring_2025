194ba323e7a7893109563fea8f9e55add0cc7b27
renekilp <rene.kilpelainen@gmail.com>
2025-01-29 22:27:47 +0200

    finished Assignment 4.c

--- Assignments/Assignment 4.c
+++ Assignments/Assignment 4.c
@@ -1,26 +1,54 @@
 #include <stdio.h>
 
-// Exercise 4 - Interger checker + function
-
-
-int number;
-int numbers[];
+// Exercise 4 - Positive numbers, average using a function *COMPLETED*
 
 int read_integer(void)
 {
-  if (number > 0) {
-    return number;
-  if (number != 1) {
-    while(getchar() != '\n');
-    printf("Invalid Input!\n");
+
+  int number;
+
+  // trying out the "input validation" provided by the exercise material pdf
+  while (1) {
+    printf("Enter positive numbers or negative to stop:\n");
+    if (scanf("%d", &number) == 1) {
+      return number;
+    }
+    printf("Invalid input. Please enter only positive numbers!\n");
+    while (getchar() != '\n');
+    // seems to be working fine :)
   }
-  return number;
 }
 
 int main(void)
 {
-  printf("Enter a positive number (or a negative number to quit:\n");
-  scanf("%d",&number);
 
+  /*
+      'count' keeps track of the positive numbers entered
+      'total' keeps track of the sum of the entered numbers (unless it's a negative which breaks the loop)
+  */
+
+  int count = 0;
+  int total = 0;
+  int number;
+  float average;
+
+  while (1) {
+    number = read_integer();
+
+    if (number < 0) {
+      break;
+    }
+    total += number;
+    count++;
+  }
+
+  // final calculations for the average
+  if (count > 0) {
+    average = (float)total / count;
+    printf("You entered %d positive numbers.\n", count);
+    printf("The average is: %.3f\n", average);
+  }
   return 0;
 }
+
+
